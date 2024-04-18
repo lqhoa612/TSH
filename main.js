1c7c5472ee6e91f50d6fc8fe8e898dc8e8772f9d
lqhoa612 <72690697+lqhoa612@users.noreply.github.com>
2024-04-18 13:44:18 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -16,17 +16,19 @@ function translatePage(language) {
         guide2: "If you have multiple middle names, enter anyhow you like just make sure to enter your first name LAST.",
         guide3: "Enter your birthdate in dd-mm-yyyy format, just the numbers, like 1st January 1111, enter 01011111.",
         guide4: "Finally just hit Enter key or click the Calculate button and your Numerology results should appear below the button.",
-        guide5: "***To learn more about your Numerology result, contact me via the email below with the result and I'll get back to you as soon as I can.",
-        guide6: "***Please email me if there is any error with the calculator or any feedback/improvement you want to share.",
+        guide5: "*** To learn more about your Numerology result, contact me via the email below with the result and I'll get back to you as soon as I can.",
+        guide6: "*** Please email me if there is any error with the calculator or any feedback/improvement you want to share.",
+        guide7: "*** Disclamer: This is a work in process!",
         footer: "&copy; 2024 Numerology Calculator by Quoc Hoa Le",
         contactHead: "Contact Information:",
 
+        yourmapLabel: "Your map: ",
         fullnameLabel: "Fullname: ",
         birthdate2Label: "Birthdate: ",
         todateLabel: "Date generated: ",
-        duongdoiLabel: "Lifepath: ",
+        duongdoiLabel: "Life path: ",
         sumenhLabel: "Destiny/Mission: ",
-        lienketduongdoisumenhLabel: "Connection (Lifepath - Destiny): ",
+        lienketduongdoisumenhLabel: "Connection (Life path - Destiny): ",
         truongthanhLabel: "Growth/Mature: ",
         linhhonLabel: "Soul/Urge: ",
         nhancachLabel: "Characteristic: ",
@@ -55,10 +57,13 @@ function translatePage(language) {
         guide2: "Nếu bạn có nhiều tên đệm, bạn nhập theo thứ tự nào cũng được, riêng tên gọi phải được nhập cuối cùng.",
         guide3: "Nhập ngày tháng năm sinh theo dd/mm/yyyy, ví dụ như ngày 1 tháng 1 năm 1111, thì nhập 01011111",
         guide4: "Cuối cùng nhấn Enter hoặc nhấp vào nút tính toán và kết quả Thần Số Học sẽ hiện ra ở dưới nút tính toán.",
-        guide5: "***Để tìm hiểu thêm về chỉ số Thần Số Học của bạn, hãy liên hệ tôi qua email bên dưới kèm theo kết quả bạn tính được, và tôi sẽ gửi cho bạn thông tin về các chỉ số sớm nhất có thể.",
+        guide5: "*** Để tìm hiểu thêm về chỉ số Thần Số Học của bạn, hãy liên hệ tôi qua email bên dưới kèm theo kết quả bạn tính được, và tôi sẽ gửi cho bạn thông tin về các chỉ số sớm nhất có thể.",
+        guide6: "*** Hãy liên hệ với tôi nếu bạn cho rằng chương trình tính có lỗi hoặc bạn muốn chia sẻ cảm nhận. Mọi đóng góp đều mang ý nghĩa rất lớn đối với tôi.",
+        guide7: "*** Chú ý: Đây là chương trình thử nghiệm!",
         footer: "&copy; 2024 Máy tính Thần Số Học do Quốc Hòa Lê phát triển",
         contactHead: "Thông Tin Liên Lạc:",
 
+        yourmapLabel: "Bản đồ của bạn: ",
         fullnameLabel: "Họ và Tên: ",
         birthdate2Label: "Ngày sinh: ",
         todateLabel: "Ngày hiện tại: ",
@@ -174,6 +179,8 @@ function reduceToSingleDigit(num, allowMaster) {
 }
 
 function calculateNumerology() {
+    clearMessages();
+    clearMap();
     // Get user inputs
     var rawName = document.getElementById('name').value;
     var name = removeAccents(rawName);
@@ -377,78 +384,107 @@ function englishIndexButtons(buttonId) {
     var mess2 = document.getElementById('message2');
     var mess3 = document.getElementById('message3');
     var mess4 = document.getElementById('message4');
+    if (prevButton == 'yourmapBtn') {
+        clearMap();
+        clearMap();
+    } else {
+        clearMessages();
+    }
+    
     switch (buttonId) {
+        case 'yourmapBtn':
+            header.textContent = "Your map"
+            displayBirthdateMap();
+            displayNameMap();
+            break;
         case 'duongdoiBtn':
-            header.textContent = "Lifepath";
-            mess1.textContent = ""
+            header.textContent = "Life path"
+            mess1.textContent = "Life path numbers reveal who you are, your strengths and weaknesses."
+            mess2.textContent = "It tells you the lesson you need to learn to be your better version."
+            mess3.textContent = "Life path number is calculated by add each numbers of your birthday and reduce to a single digit (from 1 to 9)."
+            mess4.textContent = "Exept when it is 11, 22 or 33."
             break;
         case 'sumenhBtn':
-            header.textContent = "Destiny/Mission";
-            mess1.textContent = ""
+            header.textContent = "Destiny/Mission"
+            mess1.textContent = "Destiny numbers explore what you are destined to do in your lifetime, your mission and who you are destined to become."
+            mess2.textContent = "It also points out the strengths you can use and develop to fulfill your life path."
+            
             break;
         case 'lienketduongdoisumenhBtn':
-            header.textContent = "Connection (Lifepath - Destiny/Mission)";
-            mess1.textContent = ""
+            header.textContent = "Connection (Life path - Destiny/Mission)"
+            mess1.textContent = "Connection numbers show how you can become what you are destined to be and by that, fulfill your life path."
+            
             break;
         case 'truongthanhBtn':
-            header.textContent = "Growth/Mature";
-            mess1.textContent = "";
+            header.textContent = "Growth/Mature"
+            mess1.textContent = "Growth or Mature numbers tell you how you can grow with your body, your mind, and your heart."
+            
             break;
         case 'linhhonBtn':
-            header.textContent = "Soul/Urge";
-            mess1.textContent = ""
+            header.textContent = "Soul/Urge"
+            mess1.textContent = "The soul urge numbers reveal what you would love to do, what you would want to become, what you are urging for in life."
+            mess2.textContent = "They also project how you want people to treat you."
+            
             break;
         case 'nhancachBtn':
-            header.textContent = "Characteristic";
-            mess1.textContent = ""
+            header.textContent = "Characteristic"
+            mess1.textContent = "Characteristic numbers tell you how people see you."
+            mess2.textContent = "It is sometimes called First Impression numbers."
+            mess3.textContent = "And sometimes this is not who you really are."
             break;
         case 'lienketlinhhonnhancachBtn':
-            header.textContent = "Connection (Soul/Urge - Characteristic)";
-            mess1.textContent = ""
+            header.textContent = "Connection (Soul/Urge - Characteristic)"
+            mess1.textContent = "Sometimes the way people see you is not the way you want to express."
+            mess2.textContent = "These numbers suggest you how to unify the two of them, how to live as your true self and not something else."
+            
             break;
         case 'canbangBtn':
-            header.textContent = "Balance";
-            mess1.textContent = ""
+            header.textContent = "Balance"
+            mess1.textContent = "These numbers show how you face problems in life."
+            
             break;
         case 'tuduylytriBtn':
-            header.textContent = "Rational thinking";
-            mess1.textContent = ""
+            header.textContent = "Rational thinking"
+            mess1.textContent = "These numbers reveal the way you think, how you would approach a problem in life."
+            
             break;
         case 'sucmanhtiemthucBtn':
-            header.textContent = "Subconcious ability";
-            mess1.textContent = ""
+            header.textContent = "Subconcious ability"
+            mess1.textContent = "Subconsios ability numbers tell you about what you are capable of, your speciality in your past life that is brought to this life."
+            
             break;
         case 'sothieuBtn':
-            header.textContent = "Imbalance number(s)";
-            mess1.textContent = ""
+            header.textContent = "Imbalance number(s)"
+            mess1.textContent = "Imbalance numbers are numbers whose energy is exceeding or contrasting to the standard."
+            mess2.textContent = "For example if you have a number X as one of the imbalance numbers, you might be talking too much that you cannot control what you are saying, or you are not communicating enough yet you think no one understands you."
+            
             break;
         case 'ngaysinhBtn':
-            header.textContent = "Birth day";
-            mess1.textContent = ""
+            header.textContent = "Birth day"
+            mess1.textContent = "Your natural talents are hidden behind your very own day of birth."
+            mess2.textContent = "Just reduce your day of birth to a single digit and you will have your birth day number."
+            
             break;
         case 'namcanhanBtn':
-            header.textContent = "Personal year";
-            mess1.textContent = ""
+            header.textContent = "Personal year"
+            mess1.textContent = "In Numerology, everyone has their 4 milestones and each milestone is 9 years."
+            mess2.textContent = "From the day you are born to 9 years before"
+            
             break;
         case 'thangcanhanBtn':
-            header.textContent = "Personal month";
-            mess1.textContent = ""
+            header.textContent = "Personal month"
             break;
         case 'ngaycanhanBtn':
-            header.textContent = "Personal day";
-            mess1.textContent = ""
+            header.textContent = "Personal day"
             break;
         case 'changBtn':
-            header.textContent = "Milestones";
-            mess1.textContent = ""
+            header.textContent = "Milestones"
             break;
         case 'tuoiBtn':
-            header.textContent = "Milestone ages";
-            mess1.textContent = ""
+            header.textContent = "Milestone ages"
             break;
         case 'thachthucBtn':
-            header.textContent = "Challenges";
-            mess1.textContent = ""
+            header.textContent = "Challenges"
             break;
         default:
             break;
@@ -461,81 +497,183 @@ function vietnameseIndexButtons(buttonId) {
     var mess2 = document.getElementById('message2');
     var mess3 = document.getElementById('message3');
     var mess4 = document.getElementById('message4');
+    if (prevButton == 'yourmapBtn') {
+        clearMap();
+    } else {
+        clearMessages();
+    }
+    
     switch (buttonId) {
+        case 'yourmapBtn':
+            header.textContent = "Bản đồ của bạn:"
+            displayBirthdateMap();
+            break;
         case 'duongdoiBtn':
             header.textContent = "Đường đời";
-            mess1.textContent = ""
             break;
         case 'sumenhBtn':
             header.textContent = "Sứ mệnh";
-            mess1.textContent = ""
             break;
         case 'lienketduongdoisumenhBtn':
             header.textContent = "Liên kết (Đường đời - Sứ mệnh)";
-            mess1.textContent = ""
             break;
         case 'truongthanhBtn':
             header.textContent = "Trưởng thành";
-            mess1.textContent = "";
             break;
         case 'linhhonBtn':
             header.textContent = "Linh hồn";
-            mess1.textContent = ""
             break;
         case 'nhancachBtn':
             header.textContent = "Nhân cách";
-            mess1.textContent = ""
             break;
         case 'lienketlinhhonnhancachBtn':
             header.textContent = "Liên kết (Linh hồn - Nhân cách)";
-            mess1.textContent = ""
             break;
         case 'canbangBtn':
             header.textContent = "Cân bằng";
-            mess1.textContent = ""
             break;
         case 'tuduylytriBtn':
             header.textContent = "Tư duy lý trí";
-            mess1.textContent = ""
             break;
         case 'sucmanhtiemthucBtn':
             header.textContent = "Sức mạnh tiềm thức";
-            mess1.textContent = ""
             break;
         case 'sothieuBtn':
             header.textContent = "Số thiếu";
-            mess1.textContent = ""
             break;
         case 'ngaysinhBtn':
             header.textContent = "Ngày sinh";
-            mess1.textContent = ""
             break;
         case 'namcanhanBtn':
             header.textContent = "Năm cá nhân";
-            mess1.textContent = ""
             break;
         case 'thangcanhanBtn':
             header.textContent = "Tháng cá nhân";
-            mess1.textContent = ""
             break;
         case 'ngaycanhanBtn':
             header.textContent = "Ngày cá nhân";
-            mess1.textContent = ""
             break;
         case 'changBtn':
             header.textContent = "Chặng";
-            mess1.textContent = ""
             break;
         case 'tuoiBtn':
             header.textContent = "Tuổi kết thúc chặng";
-            mess1.textContent = ""
             break;
         case 'thachthucBtn':
             header.textContent = "Thách thức";
-            mess1.textContent = ""
             break;
         default:
             break;
+    }
+}
+
+function displayBirthdateMap() {
+    const header = document.createElement('h3');
+    header.textContent = 'Birthdate map';
+
+    const birthday = document.getElementById('birthdate').value;
+    const birthdateString = birthday.replace(/\//g, '');
+
+    var frameMap = [
+        [3,6,9],
+        [2,5,8],
+        [1,4,7]
+    ];
+    const matrixContainer = document.createElement('div');
+    matrixContainer.id = 'birthdateMatrixContainer';
+    const table = document.createElement('table');
+
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 3; j++) {
+            const cell = document.createElement('td');
+            let cellContent = '';
+            for (let index = 0; index < birthdateString.length; index++) {
+                if (birthdateString[index] == frameMap[i][j]) {
+                    cellContent += birthdateString[index];
+                }
+            }
+            cell.textContent = cellContent || ' ';
+            row.appendChild(cell);
+            
+        }
+        table.appendChild(row);
+    }
+    matrixContainer.appendChild(header);
+    matrixContainer.appendChild(table);
+    bottomright.appendChild(matrixContainer);
+}
+
+function displayNameMap() {
+    const header = document.createElement('h3');
+    header.textContent = 'Name map';
+
+    var rawName = document.getElementById('name').value;
+    var name = removeAccents(rawName);
+    name = name.toLowerCase();
+    var nChar = ' ';
+    var nCharNumStorage = [];
+    for (let i = 0; i < name.length; i++) {
+        nChar = name.charAt(i);
+        if (/[a-z]/.test(nChar)) {
+            nCharNum = reduceToSingleDigit(nChar.charCodeAt(0)-96, false);
+            nCharNumStorage.push(nCharNum.toString());
+        }
+    }
+    
+    var frameMap = [
+        [3,6,9],
+        [2,5,8],
+        [1,4,7]
+    ];
+    const matrixContainer = document.createElement('div');
+    matrixContainer.id = 'nameMatrixContainer';
+    const table = document.createElement('table');
+
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 3; j++) {
+            const cell = document.createElement('td');
+            let cellContent = '';
+            for (let index = 0; index < nCharNumStorage.length; index++) {
+                if (nCharNumStorage[index] == frameMap[i][j]) {
+                    cellContent += nCharNumStorage[index];
+                }
+            }
+            cell.textContent = cellContent || ' ';
+            row.appendChild(cell);
+            
+        }
+        table.appendChild(row);
+    }
+    matrixContainer.appendChild(header);
+    matrixContainer.appendChild(table);
+    bottomright.appendChild(matrixContainer);
+}
+
+function clearMap() {
+    var matrixContainer = document.getElementById('bottomright').querySelector('div');
+    if (matrixContainer == null) return;
+    // Remove all child elements from matrixContainer
+    while (matrixContainer.firstChild != null) {
+        matrixContainer.removeChild(matrixContainer.firstChild);
+    }
+    matrixContainer.parentNode.removeChild(matrixContainer);
+}
+
+function clearMessages() {
+    var header = document.getElementById('bottomHead');
+    var mess1 = document.getElementById('message1');
+    var mess2 = document.getElementById('message2');
+    var mess3 = document.getElementById('message3');
+    var mess4 = document.getElementById('message4');
+    
+    if (header !== null && mess1 !== null && mess2 !== null && mess3 !== null && mess4 !== null) {
+        header.textContent = "";
+        mess1.textContent = "";
+        mess2.textContent = "";
+        mess3.textContent = "";
+        mess4.textContent = "";
     }
 }
 // Explanation for the idecies <---
